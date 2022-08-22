bb96e2b257af5b1a6a19dbe2b7254f6529638c9a
Harshal Kahar <93152436+harshal255@users.noreply.github.com>
2022-08-22 17:10:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+const Darkmode = () => {
+
+    document.body.style.backgroundColor = "rgb(0, 24, 5)";
+    document.getElementById('img').style.opacity = "0.2";
+    document.getElementById("myH2").style.color = "aliceblue";
+    document.getElementById('btn1').style.backgroundColor = 'chartreuse';
+    document.getElementById('btn2').style.backgroundColor = 'chartreuse';
+
+
+
+
+
+}
+
+const Lightmode = () => {
+    document.body.style.backgroundColor = "rgba(0, 218, 247, 0.779)";
+    document.getElementById('img').style.opacity = "0.8";
+    document.getElementById('btn1').style.backgroundColor = 'rgba(162, 119, 255, 0.479)';
+    document.getElementById('btn2').style.backgroundColor = 'rgba(162, 119, 255, 0.479)';
+    document.getElementById("myH2").style.color = "blue";
+
+
+
+}
+
+
+
